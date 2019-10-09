69bce06ebd501f5863405882dfdd8fbb49c43420
Lily Best <lily.m.best@gmail.com>
2019-10-09 11:17:49 -0500

    all done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
+/*Dependencies */
+import React, { Component } from 'react';
+/* Styling */
 import './App.css';
+/* Components */
+import Circles from './components/Circles/Circles'
+import CircleSelector from './components/CircleSelector/CircleSelector'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      selected: '1',
+    }
+  this.onChange = this.onChange.bind(this)
+  }
+
+  onChange = (newSelection) =>{
+    this.setState({
+    selected: newSelection
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">UNIT 4 FINAL ASSESSMENT</header>
+        <main>
+          <CircleSelector 
+          selected={this.state.selected}
+          onChange={this.onChange}
+          />
+          <Circles 
+          selected={this.state.selected}
+          />
+        </main>
+      </div>
+    );
+  }
+
 }
 
 export default App;
